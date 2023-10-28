e89323820c0ed8667bf9277bef1916b949bcd248
taylor.martinez <taylor.swot.2018@gmail.com>
2023-10-28 17:14:39 -0500

    updated seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,6 @@
-INSERT INTO department
+INSERT INTO department (department_name)
+VALUES ("Engineering"),
+        ("Finance"),
+        ("Legal");
+
+INSERT INTO role (title, salary, department_id)
