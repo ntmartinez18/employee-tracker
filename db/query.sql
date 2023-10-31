668d25e978050e500b538ca142c9f15a5f8ce06d
taylor.martinez <taylor.swot.2018@gmail.com>
2023-10-31 13:49:15 -0500

    update query.sql file

--- db/query.sql
+++ db/query.sql
@@ -6,4 +6,23 @@ SELECT * FROM department;
 USE company_db;
 SELECT * FROM roles;
 
+-- create function to view all employees
+USE company_db;
+SELECT * FROM employee;
+
 -- create function to add a department
+INSERT INTO departments (department_name)
+VALUES ('{department_name}');
+
+-- create function to add role
+INSERT INTO roles (role_title, role_salary, department_id)
+VALUES ('{role_title}', {role_salary}, {department_id});
+
+-- create function to add an employee
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('{first_name}', '{last_name}', {role_id}, {manager_id});
+
+-- create function to update employee role
+UPDATE employee
+SET role_id = {new_role_id}
+WHERE employee_id = {employee_id};
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 require('dotenv').config();
 const inquirer = require('inquirer');
 
+// Create a connection to the MySQL database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'your_mysql_username',
+    password: process.env.DB_PASSWORD,
+    database: 'company_db',
+  });
+
 
 const questions = [
     {
@@ -11,10 +20,34 @@ const questions = [
     },
 ];
 
-const { answers } = answers => {
-    console.log(answers)
-};
+function addDepartment() {
+    // Prompt the user to enter the name of the department
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'departmentName',
+          message: 'Enter the name of the department:',
+        },
+      ])
+      .then((answers) => {
+        const departmentName = answers.departmentName;
+  
+        // Insert the department into the database
+        const query = 'INSERT INTO departments (name) VALUES (?)';
+        connection.query(query, [departmentName], (err, res) => {
+          if (err) throw err;
+          console.log('Department added successfully!');
+          // Close the database connection
+          connection.end();
+        });
+      });
+  }
+  
+  // Call the addDepartment function to start adding a department
+  addDepartment();
 
 inquirer
 .prompt(questions)
 .then(answers);
+
